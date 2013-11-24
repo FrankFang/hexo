ac1cd776fac9f5e3dc98404ebee86a314ac9a484
Frank Fang <frankfang1990@gmail.com>
2013-11-25 01:04:19 +0800

    update

--- themes/light/source/js/main.js
+++ themes/light/source/js/main.js
@@ -2,4 +2,4 @@
  * Created by frank on 13-11-23.
  */
 var avatarUrl = 'http://www.gravatar.com/avatar/' + md5('frankfang1990@gmail.com');
-$('#myAvatar').src = avatarUrl;
+$('#myAvatar')[0].src = avatarUrl;
