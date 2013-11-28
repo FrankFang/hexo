9a777f7de6000c78cedff80e762970313220fa84
Frank Fang <ffang@bbtechgroup.com>
2013-11-28 14:18:51 +0800

    update

---
+++ source/_posts/2013-11-28-css-photo-frame.md
@@ -0,0 +1,6 @@
+title: CSS相框
+date: 2013-11-28 14:14:33
+tags: 编程
+---
+
+<a class="jsbin-embed" href="http://jsbin.com/AWogEFE/2/embed?output">JS Bin</a><script src="http://static.jsbin.com/js/embed.js"></script>
--- themes/light/source/js/main.js
+++ themes/light/source/js/main.js
@@ -1,5 +1,5 @@
 /**
  * Created by frank on 13-11-23.
  */
-var avatarUrl = 'http://www.gravatar.com/avatar/' + md5('frankfang1990@gmail.com');
-$('#myAvatar')[0].src = avatarUrl;
+//var avatarUrl = 'http://www.gravatar.com/avatar/' + md5('frankfang1990@gmail.com') + '?s=80';
+//$('#myAvatar')[0].src = avatarUrl;
