1b65174dd87ccbf95fc74f0a909e03720642f005
Frank Fang <frankfang1990@gmail.com>
2013-12-08 22:04:26 +0800

    update

--- themes/frank/source/js/main.js
+++ themes/frank/source/js/main.js
@@ -4,26 +4,3 @@
 //var avatarUrl = 'http://www.gravatar.com/avatar/' + md5('frankfang1990@gmail.com') + '?s=80';
 //$('#myAvatar')[0].src = avatarUrl;
 
-function doit() {
-  var xs = document.querySelectorAll(('#my_panel_wrap li .del,#my_panel_wrap li .x-to-hide'));
-  var i = 0;
-  var t = setInterval(function () {
-    xs[i].click();
-    setTimeout(function () {
-      document.querySelector('.pop_content .input-submit').click();
-      i += 1;
-      if (i >= xs.length) {
-        document.querySelector('.pagerpro .current').nextElementSibling.querySelector('a').click();
-        window.clearInterval(t);
-        setTimeout(doit, 2000)
-      }
-    }, 100)
-  }, 200);
-}
-var ttt = setInterval(function () {
-  doit();
-  if (!document.querySelector('.pagerpro .current').nextElementSibling) {
-    window.clearInterval(ttt);
-  }
-}, 10000);
-doit();
